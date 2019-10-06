65efba09ef4ca532185616cf39af470d3b8096c5
Daisaku Ejiri <deekay1122@gmail.com>
2019-10-06 15:05:16 +0900

    cart logic added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const Product = require('../models/Product');
+const Cart = require('../models/Cart');
 
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
@@ -20,7 +21,19 @@ router.get('/shop', (req, res)=>{
 });
 
 router.get('/add_to_shopping_cart/:id', (req, res)=>{
+  console.log(req.params);
   let productId = req.params.id;
+  let cart = new Cart(req.session.cart ? req.session.cart : {} );
+  Product.findOne({where: {id: productId}}).then(product=>{
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/shop');
+  })
+  .catch(err => {
+    console.log(err);
+    res.redirect('/shop');
+  });
 });
 
 module.exports = router;
